55f93aff7f04c0d93722aa45daba30609326c561
Dzmitry Kalabuk <d.kalabuk@subsquid.io>
2024-05-29 20:14:05 +0300

    Fix worker setup script

--- src/worker/setup_worker.sh
+++ src/worker/setup_worker.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -euf -o pipefail
 
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
-
-if [ "$#" -ne 2 ]
-then
-  echo "Usage: $0 <DATA_DIR> <KEY_PATH>"
-  exit 1
-fi
-
 # Plain realpath doesn't work on MacOS with non-existing files
 abspath() {
   if [ -d "$1" ]
@@ -20,8 +11,34 @@ abspath() {
   fi
 }
 
+check_deps() {
+  if ! command -v docker &>/dev/null
+  then
+    echo "Docker is not installed. Please install it and try again."
+    exit 1
+  fi
+  if ! command -v curl &>/dev/null
+  then
+    echo "Curl is not installed. Please install it and try again."
+    exit 1
+  fi
+}
+
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+if [ "$#" -ne 2 ]
+then
+  echo "Usage: $0 <DATA_DIR> <KEY_PATH>"
+  exit 1
+fi
+
+check_deps
+
 DATA_DIR=$(abspath "$1")
 KEY_PATH=$(abspath "$2")
+NETWORK=${NETWORK:-mainnet}
 
 if [ -d "$DATA_DIR" ]
 then
@@ -37,7 +54,7 @@ else
   echo "Generating a new key file in '${KEY_PATH}'."
   echo "${BOLD}Please make sure to back up the key file in a secure location.${NORMAL}"
 fi
-PEER_ID=$(docker run -u "$(id -u):$(id -g)" -v "$(dirname "${KEY_PATH}"):/host" subsquid/keygen:tethys "/host/$(basename "${KEY_PATH}")")
+PEER_ID=$(docker run -q -u "$(id -u):$(id -g)" -v "$(dirname "${KEY_PATH}"):/host" subsquid/keygen:tethys "/host/$(basename "${KEY_PATH}")")
 
 read -r -p "Provide the UDP port to listen on (default: 12345): " LISTEN_PORT
 LISTEN_PORT=${LISTEN_PORT:-12345}
@@ -60,12 +77,14 @@ KEY_PATH=${KEY_PATH}
 LISTEN_PORT=${LISTEN_PORT}
 PROMETHEUS_PORT=
 PUBLIC_ADDR=${PUBLIC_ADDR:-}
-RPC_URL=
-L1_RPC_URL=
+NETWORK=${NETWORK}
 UID=$(id -u)
 GID=$(id -g)
 EOF
 
-
 echo "Config saved to '.env'"
+
+echo "Downloading docker-compose.${NETWORK}.yaml"
+curl -sSf "https://cdn.subsquid.io/worker/docker-compose.${NETWORK}.yaml" -o docker-compose.yaml
+
 echo "Your peer ID is: ${BOLD}${PEER_ID}${NORMAL}. Now you can register it on chain."
