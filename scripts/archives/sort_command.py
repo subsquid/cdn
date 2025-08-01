58f3082cffdb54e06399f94eb6cd9d62b687d393
octo-gone <nklimin007@gmail.com>
2025-08-01 11:04:55 +0300

    feat: add dancelight

--- scripts/archives/sort_command.py
+++ scripts/archives/sort_command.py
@@ -28,7 +28,7 @@ def _run(parsed_args):
     root_p = Path(os.path.realpath(__file__)).parent.parent.parent
     with open(root_p / f"src/archives/{parsed_args.variant}.json") as f:
         archives = json.load(f)
-    archives["archives"] = sorted(archives["archives"], key=lambda x: x["id"])
+    archives["archives"] = sorted(archives["archives"], key=lambda x: x["network"])
     with open(root_p / f"src/archives/{parsed_args.variant}.json", "w") as f:
         json.dump(archives, f, indent=2, sort_keys=True)
     console.print("Done!")
